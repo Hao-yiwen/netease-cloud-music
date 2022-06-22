51a745e5ce58b42acfbc04b9fd040dbc35f59dec
郝宜文 <1552975589@qq.com>
2022-06-23 00:36:57 +0800

    123

---
+++ src/store/models/person/person.ts
@@ -0,0 +1,19 @@
+import { createModel } from '@rematch/core'
+import { Dispacth, RootModel } from '..'
+import { Person } from './person.types';
+
+export const person = createModel<RootModel>()({
+    state: {
+        person: 0
+    } as Person,
+    reducers: {
+        increament(state, payload: Person) {
+            return { ... state, person: state.person + payload.person }
+        }
+    },
+    effects: dispacth => ({
+        increamentAsync(payload: Person, state) {
+            dispacth.person.increament(payload);
+        },
+    })
+})
---
+++ src/store/store.ts
@@ -0,0 +1,8 @@
+import {init} from '@rematch/core';
+import {models} from './models'
+
+export const store = init({
+  models
+})
+
+export const { dispacth } = store
